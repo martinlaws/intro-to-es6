04ba62b3ac1359148a6f6e19b0edaf1066083e27
Martin Laws <martin.laws@indexexchange.com>
2017-11-09 16:06:46 -0500

    Updated variable reference in Object.freeze() example

--- 2-let-and-const/examples.js
+++ 2-let-and-const/examples.js
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // check devtools, this is the real deal.
 
     // you can, however, update a let variable (duh):
-    let value = 1;
-    value = 'test';
+    // let value = 1;
+    // value = 'test';
 
     // same goes for `const`, but it can NOT be updated:
     // const name = 'Martin';
@@ -19,12 +19,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // check devtools, this is the real deal.
 
     // `let` is block-scoped:
-    let weather = 'cloudy';
-
-    if (true) {
-        let weather = 'sunny';
-        // console.log(`the weather inside the if block is ${weather}`);
-    }
+    // let weather = 'cloudy';
+    //
+    // if (true) {
+    //     let weather = 'sunny';
+    //     console.log(`the weather inside the if block is ${weather}`);
+    // }
     // console.log(`the weather outisde the if is ${weather}`);
 
     // as you can see in devtools, the two winner variables are scoped to their blocks
@@ -35,14 +35,14 @@ document.addEventListener("DOMContentLoaded", () => {
         hair: 'brown',
         eyes: 'blue'
     }
-    // console.log(`Martin's initial hair colour was ${martin.hair}.`);
+    console.log(`Martin's initial hair colour was ${martin.hair}.`);
 
     // while we cannot reassign a const variable, we can update properties within it, like so:
     martin.hair = 'green';
-    // console.log(`Even though he's a const, Martin's hair is now ${martin.hair}!`);
+    console.log(`Even though he's a const, Martin's hair is now ${martin.hair}!`);
 
     // if you do need to freeze everything on a const object, you can do the following:
     const frozenMartin = Object.freeze(martin);
     frozenMartin.hair = 'purple';
-    // console.log(`now, frozenMartin's hair is always ${martin.hair}, even after we try to change it.`);
+    console.log(`now, frozenMartin's hair is always ${frozenMartin.hair}, even after we try to change it.`);
 });
