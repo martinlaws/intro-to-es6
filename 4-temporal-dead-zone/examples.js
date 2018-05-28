a38c952bdb5c9b8f77dfc32e40ab4b0fddea71a6
Martin Laws <martin.laws@ampleorganics.com>
2018-05-28 13:37:31 -0400

    Updated lesson 4 -temporal dead zone

--- 4-temporal-dead-zone/examples.js
+++ 4-temporal-dead-zone/examples.js
@@ -4,13 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(message);
     var message = 'Hello world!';
 
-    // see what happened there? the variable's existence was hoisted, but the value was not
+    // // see what happened there? the variable's existence was hoisted, but the value was not
+    // // so it really amounts to:
+    // var message = undefined;
+    // console.log(message);
+    // message = 'Hello world!';
 
     // this doesn't happen with `let` or `const`, uncomment either of the following examples:
-    // example 1
     // console.log(letMessage);
     // let letMessage = 'World? Can you hear me?';
-    // example 2
+
     // console.log(constMessage);
     // const constMessage = 'Hello from the other side!';
 });
