fbd6ca635433b87c4c07abb8cc9db68196f91290
Martin Laws <martin.laws@ampleorganics.com>
2018-05-29 12:09:07 -0400

    Updates lesson 11 - iterating over objects

---
+++ 11-for-of-with-objects/examples.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const person = {
+        name: 'Martin',
+        height: `6'2"`,
+        favouriteColour: 'lightblue'
+    }
+
+    // if you are able to use a polyfill for ES2017 features, objects can have a `.entries()` method available too
+    // which could look something like:
+
+    // for (const prop of person.entries()) {
+    //     console.log(prop);
+    // }
+
+    // // if we want to stick to what works in ES6:
+    // for (const prop of Object.keys(person)) {
+    //     const value = person[prop];
+    //     console.log(`${prop}: ${value}`);
+    // }
+
+    // // but what actually seems cleaner **for iterating through objects** is `for in`:
+    for (const prop in person) {
+        const value = person[prop];
+        console.log(`${prop}: ${value}`);
+    }
+});
